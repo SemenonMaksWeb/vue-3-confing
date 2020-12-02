5774704b842bc7da6c64570cc4559209a68d6c9f
Павел <Pavel@mail.ru>
2020-12-02 17:50:09 +0300

    настройки фул

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@ module.exports = {
   tabWidth: 2, // Таб размер
   semi: true, // точка с запятой после каждой строки
   // quoteProps: "preserve" // Коректно не работает!
-  trailingComma: 'all', // Без запятых в конце.
+  trailingComma: 'es5', // Без запятых в конце.
   bracketSpacing: true, //пробелы между скобками
   jsxBracketSameLine: true, // переносить > на следующую строчку при большом количестве атрубутов
   arrowParens: 'avoid', // в стрелочных функциях удалять скобки у 1 параметра
