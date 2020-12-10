7349d5b367062af5974c11e9f5c414fb9e4036cf
Павел <Pavel@mail.ru>
2020-12-10 17:19:29 +0300

    nuxt-ts

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,9 @@ module.exports = {
   tabWidth: 2, // Таб размер
   semi: true, // точка с запятой после каждой строки
   // quoteProps: "preserve" // Коректно не работает!
-  trailingComma: 'es5', // Без запятых в конце.
-  bracketSpacing: true, //пробелы между скобками
-  jsxBracketSameLine: true, // переносить > на следующую строчку при большом количестве атрубутов
-  arrowParens: 'avoid', // в стрелочных функциях удалять скобки у 1 параметра
-  printWidth: 80, //  длина строки не должна превышать 60 символов.
-  singleQuote: true,
+  trailingComma: "es5", // с запятыми в конце.
+  bracketSpacing: true, // пробелы между скобками
+  jsxBracketSameLine: false, // переносить > на следующую строчку при большом количестве атрубутов
+  arrowParens: "avoid", // в стрелочных функциях удалять скобки у 1 параметра
+  printWidth: 80, //  длина строки не должна превышать 80 символов.
 };
